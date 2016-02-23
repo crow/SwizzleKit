fb48e4cd758dbc34348a1ac77bdd37064c3289e2
David Crow <jdavidcrow@gmail.com>
2016-02-23 09:42:01 -0800

    Initial commit

---
+++ SwizzleKit/SwizzleKit.h
@@ -0,0 +1,20 @@
+//
+//  SwizzleKit.h
+//  SwizzleKit
+//
+//  Created by David Crow on 2/23/16.
+//  Copyright © 2016 DCrow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwizzleKit.
+FOUNDATION_EXPORT double SwizzleKitVersionNumber;
+
+//! Project version string for SwizzleKit.
+FOUNDATION_EXPORT const unsigned char SwizzleKitVersionString[];
+
+#import <SizzleKit/JRSwizzle.h>
+
+
+
